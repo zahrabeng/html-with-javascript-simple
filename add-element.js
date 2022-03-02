de72b5f1fa1b59eced4310bb049ee797c0c95aa0
Zahra <bghzahra@outlook.com>
2022-03-02 20:28:38 +0000

    changed newDiv to h2 newHeader

--- add-element.js
+++ add-element.js
@@ -1,12 +1,12 @@
 // create a new div element
-const newDiv = document.createElement("div");
+const newHeader = document.createElement("h2");
 
 // and give it some content
 const newContent = document.createTextNode("Hi there and greetings!");
 
 // add the text node to the newly created div
-newDiv.appendChild(newContent);
+newHeader.appendChild(newContent);
 
 // add the newly created element and its content into the DOM
 const existingDiv = document.getElementById("my-target-element");
-document.body.appendChild(newDiv, existingDiv);
+document.body.appendChild(newHeader, existingDiv);
